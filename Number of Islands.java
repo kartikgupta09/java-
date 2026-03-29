6c886afb9217614d68a5473ee200c3e543d4143f
Kartik Gupta <kartik.gupta@Kartik-Gupta-GDKW3TVX4N.local>
2026-03-29 15:24:06 +0530

    question_200_leetcode

---
+++ Number of Islands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        int ans =0;
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == '1'){
+                    bfs(i,j,r,c,grid);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public void bfs(int i, int j, int r, int c,char[][] grid){
+        if(i<0 || j<0 || i>=r || j>=c || grid[i][j] =='0'){
+            return;
+        }
+        grid[i][j] = '0';
+        bfs(i+1,j,r,c,grid);
+        bfs(i-1,j,r,c,grid);
+        bfs(i,j+1,r,c,grid);
+        bfs(i,j-1,r,c,grid);
+    }
+}
